b7d7f46f454c79f4c82375b0f31cf0affdc8d377
Abdelaali Es salmi <aes-salm@e1r5p4.1337.ma>
2020-01-15 20:32:58 +0100

    Wistia Downloader

--- wistia-downloader.py
+++ wistia-downloader.py
@@ -2,17 +2,30 @@
 import os
 import time
 import sys
-
+color_green = "\033[1;32;40m"
+color_originale = "\033[0;37;40m"
+color_yellow = "\033[1;33;40m"
+color_red = "\033[1;31;40m"
 video_index = 1
+def logo():
+	sprint("""\033[1;33;40m
+		 _  _  _ _           _          _____                    
+		| || || (_)     _   (_)        (____ \                   
+		| || || |_  ___| |_  _  ____    _   \ \ ___  _ _ _ ____  
+		| ||_|| | |/___)  _)| |/ _  |  | |   | / _ \| | | |  _ \ 
+		| |___| | |___ | |__| ( ( | |  | |__/ / |_| | | | | | | |
+		 \______|_(___/ \___)_|\_||_|  |_____/ \___/ \____|_| |_|
+	\033[0;37;40m""", 0.1)
 
-def sprint(string):
+
+def sprint(string, tm=10):
     for c in string + '\n' :
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(10 / 100)
+        time.sleep(tm / 100)
 
 
-def html_handle(file_name):
+def html_page_handle_and_download(file_name):
 	print("\033[1;32;40m")
 	global video_index
 	with open(file_name, "r+") as file:
@@ -30,21 +43,39 @@ def html_handle(file_name):
 		os.system("rm -rf {}".format(file_name))
 	print("\033[0;37;40m")
 
+
+def download_folder():
+	try:
+		username = os.path.expanduser("~").split('/')[2]
+		sprint("\033[1;32;40mPlease enter the PATH of download folder: \033[0;37;40m", 2)
+		sprint("\033[1;31;40m\t\tExample: \033[0;37;40m/Users/{}/Desktop".format(username), 2)
+		sprint("\033[1;31;40m\t\tExample: \033[0;37;40m/Volumes/Storage/goinfre/{}".format(username), 2)
+
+		folder_path = input("PATH: \033[1;33;40m")
+		print("\033[0;37;40m")
+		folder_path += "/wistia-downloader/"
+		os.system("mkdir {} 2>/dev/null".format(folder_path))
+		os.system("cp video_ID.txt {} 2>/dev/null".format(folder_path))
+		os.chdir("{}".format(folder_path))
+		sprint("\033[1;33;40mOpening the download folder...\033[0;37;40m", 2)
+		os.system("open {}".format(folder_path))
+	except FileNotFoundError:
+		print("\033[1;31;40mThe PATH does not exist !! try again\033[0;37;40m")
+		time.sleep(3.5)
+		main()
+
+
 def main():
 	os.system("clear")
-	username = os.path.expanduser("~").split('/')[2]
-	os.system("mkdir /Volumes/Storage/goinfre/$(whoami)/wistia-downloader")
-	os.system("cp video_ID.txt /Volumes/Storage/goinfre/{}/wistia-downloader/".format(username))
-	os.chdir("/Volumes/Storage/goinfre/{}/wistia-downloader".format(username))
+	logo()
+	download_folder()
 	url = "http://fast.wistia.net/embed/iframe/"
 	i = 0
-	sprint("\033[1;33;40mOpening the download folder...\033[0;37;40m")
-	os.system("open /Volumes/Storage/goinfre/{}/wistia-downloader/".format(username))
 	with open("video_ID.txt", "r+") as video_id:
 		for id in video_id:
 			os.system("curl -O {}".format(url + id))
 			print("Video " + str(i + 1) + "--------------------------------------------------------------------------")
-			html_handle(id[:-1])
+			html_page_handle_and_download(id[:-1])
 			print("-----------------------------------------------------------------------------------")
 			i+=1 
 

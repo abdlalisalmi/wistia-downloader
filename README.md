ab36d6e37c4d7f2e8aaaf6da56e915ff65dca4ab
abdlalisalmi <abdlalisalmi@gmail.com>
2020-05-27 17:03:24 +0100

    Support windows

--- README.md
+++ README.md
@@ -4,18 +4,24 @@ Python script to download from wistia player
 ## Requirements
 to run the program without problems you have to make sure you have this things in your machine
 > - python3
-> - curl command
+> - requests
 
 ## How to use
 
-### step 1: (Cloning the script)
+### step 1: (Install requests)
+>pip install requests==2.20.0
+
+### step 2: (Cloning the script)
 >git clone https://github.com/abdlalisalmi/wistia-downloader.git</br>
 >cd wistia-downloader</br>
-### step 2: (Grabbing the videos id)
+
+### step 3: (Grabbing the videos id)
 ![](https://media.giphy.com/media/YkJhH3iHcuXNaeRBCR/giphy.gif)
-### step 3: (adding IDs in video_ID.txt)
+
+### step 4: (adding IDs in video_ID.txt)
 ![](https://i.imgur.com/tsfAbAD.png)
-### step 4: (Execute the script)
+
+### step 5: (Execute the script)
 >python3 wistia-downloader.py</br>
 
 ## Created by 
--- wistia-downloader.py
+++ wistia-downloader.py
@@ -1,9 +1,14 @@
-
 import os
 import time
 import sys
+import requests
+import getpass
+from shutil import copy
+from clint.textui import progress # for the progress effects
+
 
 video_index = 1
+
 def logo():
 	sprint("""\033[1;33;40m
 		 _  _  _ _           _          _____                    
@@ -14,13 +19,22 @@ def logo():
 		 \______|_(___/ \___)_|\_||_|  |_____/ \___/ \____|_| |_|
 	\033[0;37;40m""", 0.1)
 
-
 def sprint(string, tm=10):
     for c in string + '\n' :
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(tm / 100)
 
+def download_video(url):
+	video_name = url.split('/')[-1]
+	reponse = requests.get(url, stream=True)
+	with open(video_name, 'wb') as video:
+		total_length = int(reponse.headers.get('content-length'))
+		for chunk in progress.bar(reponse.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+			if chunk:
+				video.write(chunk)
+				video.flush()
+	return video_name
 
 def html_page_handle_and_download(file_name):
 	print("\033[1;32;40m")
@@ -39,41 +53,61 @@ def html_page_handle_and_download(file_name):
 			sprint("\033[1;31;40mYour video id is not valid\033[0;37;40m", 2)
 		else:
 			new_video_url = video_url.split("url")[1].split(',')[0].split('"')[2]
-			os.system("curl -O {}".format(new_video_url))
-			video_name = video_url.split("url")[1].split(',')[0].split('"')[2].split("/")[4]
-			video_name_and_index = "Video " + str(video_index) + ".mp4"
+
+			video_name = download_video(new_video_url)
+
+			video_name_and_index = "Video{}.mp4".format(str(video_index))
 			video_index += 1
 			os.rename(video_name, video_name_and_index)
-		os.system("rm -rf {}".format(file_name))
-	print("\033[0;37;40m")
 
+	os.remove(file_name)	
+	print("\033[0;37;40m")
 
 def download_folder():
 	try:
-		username = os.path.expanduser("~").split('/')[2]
+		username = getpass.getuser()
 		sprint("\033[1;32;40mPlease enter the PATH of download folder: \033[0;37;40m", 2)
 		sprint("\033[1;31;40m\t\tExample: \033[0;37;40m/Users/{}/Desktop".format(username), 2)
 		sprint("\033[1;31;40m\t\tExample: \033[0;37;40m/Volumes/Storage/goinfre/{}".format(username), 2)
 
 		folder_path = input("PATH: \033[1;33;40m")
 		print("\033[0;37;40m")
-		folder_path += "/wistia-downloader/"
-		os.system("mkdir {} 2>/dev/null".format(folder_path))
-		os.system("cp video_ID.txt {} 2>/dev/null".format(folder_path))
+
+		slash = '/' if '/' in folder_path else '\\'
+		if folder_path[-1] == slash:
+			folder_path += "wistia-downloader"
+		else:
+			folder_path += slash + "wistia-downloader"
+
+		os.mkdir(folder_path)
+		copy('video_ID.txt', folder_path)
+
 		os.chdir("{}".format(folder_path))
-		sprint("\033[1;33;40mOpening the download folder...\033[0;37;40m", 2)
-		os.system("open {}".format(folder_path))
+		sprint("\033[1;33;40mYou will find the videos in  {}\033[0;37;40m".format(folder_path), 2)
+
 	except FileNotFoundError:
 		print("\033[1;31;40mThe PATH does not exist !! try again\033[0;37;40m")
 		sprint(".....", 100)
 		main()
 
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk:
+                f.write(chunk)
+    return local_filename
+
 
 def main():
-	os.system("clear")
+	os.system('cls' if os.name == 'nt' else 'clear')
 	logo()
+
 	download_folder()
+
 	url = "http://fast.wistia.net/embed/iframe/"
+
 	i = 0
 	with open("video_ID.txt", "r+") as video_id:
 		if os.stat("video_ID.txt").st_size == 0:
@@ -82,11 +116,18 @@ def main():
 			sprint(".....", 100)
 			main()
 		for id in video_id:
-			os.system("curl -O {}".format(url + id))
+
+			if '\n' in id :
+				new_id = id[:-1]
+			else:
+				new_id = id
+
+			video_url = url + new_id
+			local_filename = download_file(video_url)
 			print("Video ID " + str(i + 1) + "--------------------------------------------------------------------------")
-			html_page_handle_and_download(id.rstrip())
-			print("-----------------------------------------------------------------------------------")
-			i+=1 
+			html_page_handle_and_download(local_filename)
+			i+=1
+            
 
 	
 
